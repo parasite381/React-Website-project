5f3ee3fa688f1081b4edb59b90eee93d4f9f16ec
secret <secret@gmail.com>
2026-07-28 14:46:15 +0800

    correct the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { HashRouter } from 'react-router-dom'
 import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import {Provider} from "react-redux"
 import store from "./store/store.js"
@@ -83,7 +84,10 @@ const router=createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <RouterProvider router={router}/>
+      <HashRouter>
+        <App/>
+      </HashRouter>
+    {/* <RouterProvider router={router}/> */}
     </Provider>
   </StrictMode>,
 )
